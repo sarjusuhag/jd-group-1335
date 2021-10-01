6929ff0d22583ef56be06513e5b0c93b40cc336b
Harrison Zhu <hzhu359@gatech.edu>
2021-10-01 14:55:28 -0400

    tokenization file created

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+from os import write
 import nltk
+import re
+
+from nltk.util import tokenwrap
 
 # download tokenization model
 nltk.download("punkt")
 
-# read text
+# clean text
 text = None
-with open("text.txt", encoding="cp1252") as file:
-    # just read 100 bytes for now
-    text = file.read(10000)
+with open("frederickdouglasstext.txt", encoding="cp1252") as file:
+    text = file.read()
+    # ignores special characters such as �
+    # lowercases all characters
+    text = text.encode("ascii", "ignore").decode().lower()
 
 tokens = nltk.word_tokenize(text)
-print(tokens)
+print(tokens[:1000])
+
+regexp = re.compile("[^A-Za-z0-9-]")
+
+with open("tokens.txt", "w") as outfile:
+    outfile.write("\n".join([token for token in tokens if not regexp.search(token)]))
 
 # how to deal with special characters? (ÊÊÊÊÊÊÊÊ)
 # then do a set intersection of text and known words w/ words that are lowercased
