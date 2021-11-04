be65ad2a8f06a50ee88688c21290e94b733a5067
Harrison Zhu <hzhu359@gatech.edu>
2021-11-04 01:33:55 -0400

    initial words to record finished

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ def tokenize():
 
     # clean text
     text = None
-    with open("./assets/frederickdouglasstext.txt", encoding="cp1252") as file:
+    with open("./assets/frederick_douglass_text.txt", encoding="cp1252") as file:
         text = file.read()
         # ignores special characters such as �
         # lowercases all characters
         text = text.encode("ascii", "ignore").decode().lower()
 
     tokens = nltk.word_tokenize(text)
-    # print(tokens[:1000])
 
     # containing invalid chars
     regexp1 = re.compile("[^A-Za-z0-9-']")
@@ -32,9 +31,6 @@ def tokenize():
     )
     validWords = map(lambda x: x.lower(), validWords)
 
-    # with open("unfiltered.txt", "w") as outfile:
-    #     outfile.write("\n".join(validWords))
-
     # combine apostrophes
     apostropheWords = []
     for word in validWords:
@@ -42,12 +38,28 @@ def tokenize():
             apostropheWords[-1] += word
         else:
             apostropheWords.append(word)
-    validWords = set(apostropheWords)
-    print(validWords)
+    validWords = sorted(set(apostropheWords))
 
     with open("tokens.txt", "w") as outfile:
         # outfile.write("\n".join([token for token in tokens if not regexp.search(token)]))
         outfile.write("\n".join(validWords))
 
 
+def write_nonrecorded():
+    with open("tokens.txt", "r") as token_file, open(
+        "./assets/recorded_words.txt", "r"
+    ) as recorded_words_file:
+        tokens = set([line.rstrip() for line in token_file.readlines()])
+        recorded_words = set(
+            [line.rstrip("\n +") for line in recorded_words_file.readlines()]
+        )
+
+        to_record = sorted(tokens.difference(recorded_words))
+
+        with open("to_record_words.txt", "w") as to_record_file:
+            to_record_file.write("\n".join(to_record))
+
+
 tokenize()
+write_nonrecorded()
+print("done!")
