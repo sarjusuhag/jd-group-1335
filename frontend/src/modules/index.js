33b257a9595e0a9ec4e0bf7028390b03bf9e4bbc
sarjusuhag <89706507+sarjusuhag@users.noreply.github.com>
2021-11-29 23:57:01 -0500

    The reader connected through epub

---
+++ frontend/src/modules/index.js
@@ -0,0 +1,6 @@
+import EpubView from './EpubView/EpubView'
+import EpubViewStyle from './EpubView/style'
+import ReactReader from './epubreader/epubreader'
+import ReactReaderStyle from './epubreader/style'
+
+export { EpubView, EpubViewStyle, ReactReader, ReactReaderStyle }
